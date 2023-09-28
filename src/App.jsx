542bb8c21ef3df45c83e0aaba8d9adc2ab8a5b88
Jlouisa <jonathan.louisa@gmail.com>
2023-09-29 01:51:04 +0200

    Add fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
 import "./styles/App.css";
+import { v4 as uuidv4 } from "uuid";
+import { useState, ueseEffect } from "react";
 // import FetchAPIData from "./components/FetchAPIData.jsx";
 
-class Pokemon {
-  constructor(name, imgURL) {
-    this.name = name;
-    this.imgURL = imgURL;
-  }
-}
 function App() {
-  async function FetchData(apiKeyTest) {
-    const url = "https://pokeapi.co/api/v2/";
-    const response = await fetch(url + apiKeyTest, { method: "GET", mode: "cors" });
-    const data = await response.json();
-    const pokemonVar = new Pokemon(data.name, data.sprites.front_default);
-    return pokemonVar;
+  const [pokemonArr, setPokemonArr] = useState([]);
+
+  class Pokemon {
+    constructor(name, url) {
+      this.name = name;
+      this.url = url;
+      this.health = 0;
+      this.id = uuidv4();
+    }
   }
 
-  let FetchDataInfo = FetchData("pokemon/ditto").then((result) => {
-    new Pokemon(result.name, result.sprites.front_default);
-  });
-  console.log(FetchDataInfo);
+  async function getData() {
+    const url = "https://pokeapi.co/api/v2/";
+    const apiKey = "pokemon?limit=100&offset=0";
+
+    const data = await fetch(url + apiKey, { method: "GET", mode: "cors" });
+    const result = await data.json();
+    return createPokemonArr(result.results);
+  }
+
+  async function createPokemonArr(arr) {
+    const fullArr = await arr.map((pokemon) => {
+      return new Pokemon(pokemon.name, pokemon.url);
+    });
+    console.log(fullArr);
+    return fullArr;
+  }
+
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  async function createGameArr(arr) {
+    let i = 0;
+    const gameArr = [];
+    const indArr = [];
+    while (i != 24) {
+      const randomNum = getRandomNumber(0, 99);
+      if (!indArr.includes(randomNum)) {
+        indArr.push(randomNum);
+        gameArr.push(arr[randomNum]);
+        i++;
+      }
+      if (i >= 50) {
+        break;
+      }
+    }
+    console.log(gameArr);
+    return gameArr;
+  }
+
+  const info = createGameArr(getData());
+  console.log(info);
 
   return (
     <>
